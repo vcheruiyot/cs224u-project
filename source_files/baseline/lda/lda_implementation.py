efc57d8d3470c752c6e9e00866141b2ffa2cddc2
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2018-06-07 00:13:31 -0700

    edits

--- source_files/baseline/lda/lda_implementation.py
+++ source_files/baseline/lda/lda_implementation.py
@@ -9,6 +9,8 @@ import _pickle
 import gensim
 import gensim.corpora as corpora
 import os
+import pprint
+from gensim.models import CoherenceModel
 
 class lda_model:
 	def __init__(self):
@@ -17,14 +19,27 @@ class lda_model:
 	def load_data(self, train, dev):
 		#load pickle
 		print("loading pickles")
-		path = os.path.join("../../feature_groups/lda_pickles", train)
-		with open(path, "rb") as f:
-			self.raw_docs = _pickle.load(f)
-		path = os.path.join("../../feature_groups/lda_pickles", dev)
-		with open(path, "rb") as f:
-			self.dev = _pickle.load(f)
-		self.id2word = corpora.Dictionary(self.raw_docs)	
 
+		# path = os.path.join("../../feature_groups/lda_pickles", train)
+		# with open(path, "rb") as f:
+		# 	self.raw_docs = _pickle.load(f)
+		# path = os.path.join("../../feature_groups/lda_pickles", dev)
+		# with open(path, "rb") as f:
+		# 	self.dev = _pickle.load(f)
+
+
+		self.raw_docs = matrix_dev.matrix_dev('trainshort').raw_docs()
+		path = os.path.join("../../feature_groups/lda_pickles", 'raw_short')
+		with open(path, "wb") as f:
+			_pickle.dump(self.raw_docs, f)
+	    # path = os.path.join("../../feature_groups/lda_pickles", 'dev')
+	    # with open(path, "wb") as f:
+	    #     _pickle.dump(dev, f)
+
+		self.data_lemmatized = [i.split() for i in self.raw_docs]
+		self.id2word = corpora.Dictionary(self.data_lemmatized)	
+		self.corpus = [self.id2word.doc2bow(line) for line in self.data_lemmatized]
+	
 
 	def write_to_pickle(self, dir, name, obj):
 		print("writing "+name+" to pickle")
@@ -47,12 +62,13 @@ class lda_model:
 		tf_vectorizer = TfidfVectorizer(max_features=self.no_features) #stop_words='english')
 		tf = tf_vectorizer.fit_transform(self.raw_docs)
 		tf_feature_names = tf_vectorizer.get_feature_names()
-		self.write_to_pickle("../../feature_groups/lda_pickles", 'tf_vectorizer', tf)
+		#self.write_to_pickle("../../feature_groups/lda_pickles", 'tf_vectorizer', tf)
 		return tf
 
 	def lda(self):
-		print("Starting grid search")
-		lda_model = gensim.models.ldamodel.LdaModel(corpus=self.load_tfidf(),
+		print("Starting lda")
+		#corpus = self.tf_model().todense()
+		lda_model = gensim.models.ldamodel.LdaModel(corpus=self.corpus,
                                            id2word=self.id2word,
                                            num_topics=70, 
                                            random_state=100,
@@ -62,6 +78,14 @@ class lda_model:
                                            alpha='auto',
                                            per_word_topics=True)
 		pprint(lda_model.print_topics())
+		doc_lda = lda_model[corpus]
+		#calculate perplexity or how good the model is
+		print('perplexity: ', lda_model.log_perplexity(corpus))
+		#compute the coherence score
+		coherence_model_lda = CoherenceModel(model=lda_model, texts=self.data_lemmatized, dictionary=self.id2word, coherence='c_v')
+		coherence_lda = coherence_model_ld.get_coherence()
+		print('Coherence score ', coherence_lda)
+
 		#define search params
 		"""
 		search_params = {'n_components':[70, 140], 'learning_decay':[0.7, 0.9]}
